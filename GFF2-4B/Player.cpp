c698bbdd5f6f828a8be82c462c1932cbf1400069
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2022-11-10 17:15:57 +0900

    プレイヤー移動のみ

--- GFF2-4B/Player.cpp
+++ GFF2-4B/Player.cpp
@@ -1,5 +1,7 @@
 #include"DxLib.h"
 #include"Player.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 Player::Player(){
 	g_Movement_speed = 0;
@@ -8,19 +10,25 @@ Player::Player(){
 	g_PlayerX = 200;
 	g_PlayerY = 200;
 	image_number = 0;
+	image_Type = 0;
+	g_direction = FALSE;
 	LoadDivGraph("images/Player/human.png", 4, 4, 1, 30, 30, g_Playerimage);
 }
 
 
 void Player::Movement() {
 	//g_NowKey=GetJoypadInputState(DX_INPUT_PAD1); //入力キー保存
-	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_LEFT) --g_PlayerX; //左
-	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_RIGHT) ++g_PlayerX; //右
-	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_UP) --g_PlayerY; //上
-	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_DOWN) ++g_PlayerY; //下
+	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_LEFT) --g_PlayerX, ++image_number , g_direction = TRUE; //左
+	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_RIGHT) ++g_PlayerX, ++image_number, g_direction = FALSE; //右
+
+	if (image_number > 11) {
+		if (image_number % 7 == 0)image_Type++;
+	}
+	if (image_Type >= 4) image_Type = 0, image_number = 0;
 }
 
 void Player::Draw() const
 {
-	DrawGraph(g_PlayerX, g_PlayerY, g_Playerimage[image_number], TRUE);
+	//DrawGraph(g_PlayerX, g_PlayerY, g_Playerimage[image_Type], TRUE);
+	DrawRotaGraph(200 + (g_PlayerX + 15), g_PlayerY + 30, 1.0, M_PI / 180 * 0, g_Playerimage[image_Type], TRUE, g_direction);
 }
--- GFF2-4B/Player.h
+++ GFF2-4B/Player.h
@@ -10,16 +10,14 @@ private:
 	int g_PlayerY; //プレイヤー座標Y
 	int g_Movement_speed; //プレイヤー移動スピード
 	int g_Playerimage[4]; //プレイヤー画像
-	int image_number; //プレイヤーイメージ画像番号
+	int image_number; //プレイヤー画像切り替えカウント
+	int image_Type; //プレイヤー画像要素数指定
+	bool g_direction; //左を向いているのかのフラグ
+
 public:
 	Player(); //コンストラクタ
 	void Draw() const;
 	void Movement(); //プレイヤー移動処理
 	void jump(); //プレイヤーのジャンプ処理
-
-	
-
-
-
 };
 
