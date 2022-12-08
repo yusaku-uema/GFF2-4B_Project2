7b78f0ca3c9728aa746783273b88b1a857703fcc
katoushiki <106945582+katoushiki@users.noreply.github.com>
2022-12-08 10:32:27 +0900

    ｓｔ

--- GFF2-4B/main.cpp
+++ GFF2-4B/main.cpp
@@ -116,6 +116,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 			credit.Draw(); //クレジット
 			break;
 		case 6:
+			help.Draw();
 			ranking.DrawRanking(); //ランキング表示
 			break;
 		case 7:
@@ -175,6 +176,8 @@ int LoadImages()
 	if (LoadDivGraph("images/team/block.png", 7, 7, 1, 30, 30, g_block_image) == -1) return -1;
 	if (LoadDivGraph("images/Player/player.png", 4, 4, 1, 25, 25, g_player_image) == -1) return -1;
 	if (LoadDivGraph("images/team/item_cursor1.png", 3, 3, 1, 100, 100, g_item_cursor_image) == -1) return -1;
+	if (LoadDivGraph("images/team/item1.png", 5, 5, 1, 30, 30, g_item_image) == -1) return -1;
+	int a = 0;
 }
 
 /***********************************************
