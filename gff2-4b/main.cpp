02cd46d5f6b5111af29680d334a972de2ab9fe74
erimaki1844 <106945196+erimaki1844@users.noreply.github.com>
2022-12-28 00:00:32 +0900

    バグ修正

--- gff2-4b/main.cpp
+++ gff2-4b/main.cpp
@@ -10,6 +10,7 @@
 #include"Ranking.h"
 #include"Help.h"
 #include"InputRankingScene.h"
+#include"DrawRankingScene.h"
 
 
 
@@ -61,6 +62,7 @@ GameClear gameclear;
 RANKING ranking;
 Help help;
 InputRanking inputranking;
+DrawRanking drawranking;
 
 
 /***********************************************
@@ -73,6 +75,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	SetMainWindowText("掘れ掘れダンジョン君");
 	SetGraphMode(1280, 720, 16);  //画面サイズ
 	ChangeWindowMode(TRUE);		// ウィンドウモードで起動
+
+	if (ranking.ReadRanking() == -1)  return  -1;    //ランキングデータの読み込み
 
 	if (DxLib_Init() == -1) return -1;	// DXライブラリの初期化処理
 	if (LoadImages() == -1) return -1; //画像読込み
@@ -88,7 +92,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	while (ProcessMessage() == 0 && g_forcedtermination != true && g_GameState != 999) {
 
 		if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_7) {
-			g_forcedtermination = true;//強制終了
+			//g_forcedtermination = true;//強制終了
+			SetGameState(3);
 		}
 
 		timebaseloopexecuter.TimeAdjustment();// fps管理
@@ -123,8 +128,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 			break;
 		case 9:
 			/*keyconfigscene.Update();*/
+			break;
 		case 10:
 			inputranking.Update();
+			break;
+		case 11:
+			inputranking.Draw();
 			break;
 		}
 
